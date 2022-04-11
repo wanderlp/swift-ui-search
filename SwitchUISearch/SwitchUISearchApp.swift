f410ab31d264e7cd14fe9fbfb17d0180460d8944
Wanderson <lopez.wanderson@gmail.com>
2022-04-11 11:41:42 -0600

    Initial Commit

---
+++ SwitchUISearch/SwitchUISearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwitchUISearchApp.swift
+//  SwitchUISearch
+//
+//  Created by Wanderson López on 11/04/22.
+//
+
+import SwiftUI
+
+@main
+struct SwitchUISearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
